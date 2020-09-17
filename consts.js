7821f7cea6f48175315d1b60a0a78b2f73717ab8
Sakiskid <tylerdrew.home@gmail.com>
2020-09-17 16:38:38 -0500

    created consts.js, imported to have constant names and text templates for each section

---
+++ consts.js
@@ -0,0 +1,12 @@
+exports.imageName = "Image";
+exports.linkName = "Deployed Link";
+exports.updatedDateName = "Last Updated Date";
+exports.tableOfContentsName = "Table of Contents";
+exports.installationName = "Installation";
+exports.usageName = "Usage";
+exports.currentFeaturesName = "Current Features";
+exports.plannedFeaturesName = "Planned Features";
+exports.liscenseName = "Liscense";
+exports.contributingName = "Contributing";
+exports.testsName = "Tests";
+exports.questionsName = "Questions";
--- index.js
+++ index.js
@@ -1,21 +1,86 @@
 const fs = require("fs");
 const inquirer = require('inquirer');
+const consts = require('./consts');
 
 function startPrompt() {
+    let optionsSections = [];
 
     inquirer
         .prompt([
             {
-                name: "index",
+                name: "sectionsPrimary",
                 type: "checkbox",
                 message: "What sections would you like to include in your README?",
                 choices: [
-                    'Last Updated Date', 'Table Of Contents', 'Installation',
-                    'Usage', 'Liscense', 'Contributing', 'Tests', 'Questions'
+                    consts.imageName, consts.linkName, consts.updatedDateName, consts.tableOfContentsName
+                ]
+            },
+            {
+                name: "sectionsSecondary",
+                type: "checkbox",
+                message: "Sections continued...",
+                choices: [
+                    consts.installationName, consts.usageName, consts.currentFeaturesName, consts.plannedFeaturesName
+                ]
+            },
+            {
+                name: "sectionsTertiary",
+                type: "checkbox",
+                message: "Sections continued...",
+                choices: [
+                    consts.liscenseName, consts.contributingName, consts.testsName, consts.questionsName
                 ]
             }
         ])
+        .then( (answers) => {
+            // Put all of the answers together
+            for (let answer in answers) {
+                optionsSections = optionsSections.concat(answers[answer]);
+            }
+        })
+        .then( () => {
+            handleSectionCreation(optionsSections)
+        })
+        .catch ((err) => {
+            console.error("ERROR! || ", err);
+        }) 
+}
 
+function handleSectionCreation(sections){
+    let finishedText = "";
+
+    for(sectionName in sections) {
+        let thisSectionName = sections[sectionName];
+
+        switch(thisSectionName) {
+            case consts.imageName:
+                break;
+            case consts.imageName:
+                break;
+            case consts.imageName:
+                break;
+            case consts.imageName:
+                break;
+            case consts.imageName:
+                break;
+            case consts.imageName:
+                break;
+            case consts.imageName:
+                break;
+            case consts.imageName:
+                break;
+            case consts.imageName:
+                break;
+            case consts.imageName:
+                break;
+        }
+    }
+}
+
+function fillSectionWithInfo(section) {
+    inquirer.prompt([
+
+    ])
 }
 
 startPrompt();
